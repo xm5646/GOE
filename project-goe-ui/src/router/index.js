27548462bb4e60477aa67a79cb3a4dbf3aaac589
lixiaoming <lixiaoming1@sgitg.sgcc.com.cn>
2018-01-02 21:02:55 +0800

    add ui project

---
+++ project-goe-ui/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    }
+  ]
+})
