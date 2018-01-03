edfb2b5778039315c098249c2c95824a4824e65f
ccww552017 <ccww552010@126.com>
2018-01-03 14:38:42 +0800

    add EarningAduitController.java and EarningLogController.java by ccww552010@126.com

---
+++ project-geo-dbserver/src/main/java/com/project/goe/projectgeodbserver/controller/EarningAuditController.java
@@ -0,0 +1,10 @@
+package com.project.goe.projectgeodbserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/eAduit")
+public class EarningAuditController {
+	
+}
