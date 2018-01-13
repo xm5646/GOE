517e71625f5cafeb296a4f4c558c50ffbae649b3
ccww552017 <ccww552010@126.com>
2018-01-14 00:05:02 +0800

    modify UserController.java file

--- project-geo-dbserver/src/main/java/com/project/goe/projectgeodbserver/controller/UserController.java
+++ project-geo-dbserver/src/main/java/com/project/goe/projectgeodbserver/controller/UserController.java
@@ -36,13 +36,13 @@ public class UserController {
 	@Transactional
 	public RetMsg saveUser(@ModelAttribute User user) {
 		String defaultPassword = "test";
-		
+
 		try {
 			user.setCreateTime(new Date());
-			
-			if(user.getPassword() == null) {
+
+			if (user.getPassword() == null) {
 				user.setPassword(MD5Util.encrypeByMd5(defaultPassword));
-			}else {
+			} else {
 				user.setPassword(MD5Util.encrypeByMd5(user.getPassword()));
 			}
 			user.setParentId(user.getParentId());
@@ -82,23 +82,33 @@ public class UserController {
 			throw new RuntimeException("用户删除失败!");
 		}
 	}
-/**************************用户查询方法 *******************************/
-	
+
+	/************************** 用户查询方法 *******************************/
+
 	// 根据用户名查找用户信息
 	@RequestMapping("/findByAccount/{account}")
 	public User findUserByAccount(@PathVariable String account) {
-		return this.userService.findByAccount(account);
+		try {
+			return this.userService.findByAccount(account);
+		} catch (Exception e) {
+			throw new RuntimeException(e.getMessage());
+		}
 	}
-	
+
 	// 基于单个关键字进行分页查询：默认按照userId字段升序查询；默认显示第一页；默认每页显示5条数据
 	@GetMapping("/findUsersBySort")
-	public Page<User> findAllUserBySort(@RequestParam(value="pageNum",defaultValue="0",required=false) int pageNum,
-			@RequestParam(value="size",defaultValue="5",required=false) int size,
-			@RequestParam(value="keyword",required=false,defaultValue="userId") String keyword) {
-		Sort sort = new Sort(Direction.ASC,keyword);
-		Pageable pageable = new PageRequest(pageNum,size,sort);
-		
-		return this.userService.findAllUserBySort(pageable);
+	public Page<User> findAllUserBySort(
+			@RequestParam(value = "pageNum", defaultValue = "0", required = false) int pageNum,
+			@RequestParam(value = "size", defaultValue = "5", required = false) int size,
+			@RequestParam(value = "keyword", required = false, defaultValue = "userId") String keyword) {
+		try {
+			Sort sort = new Sort(Direction.ASC, keyword);
+			Pageable pageable = new PageRequest(pageNum, size, sort);
+
+			return this.userService.findAllUserBySort(pageable);
+		} catch (Exception e) {
+			throw new RuntimeException(e.getMessage());
+		}
 	}
-	
+
 }
