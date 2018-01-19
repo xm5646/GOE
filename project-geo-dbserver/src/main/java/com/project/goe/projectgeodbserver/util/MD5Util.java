f34dcafa08ee793a0ad14240924887de0bff8f64
lixiaoming <lixiaoming1@sgitg.sgcc.com.cn>
2018-01-19 19:58:00 +0800

    reset password update

--- project-geo-dbserver/src/main/java/com/project/goe/projectgeodbserver/util/MD5Util.java
+++ project-geo-dbserver/src/main/java/com/project/goe/projectgeodbserver/util/MD5Util.java
@@ -1,23 +1,30 @@
 package com.project.goe.projectgeodbserver.util;
 
-import java.io.UnsupportedEncodingException;
+ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import org.springframework.util.Base64Utils;
 
 public class MD5Util {
-	private MD5Util() {}
-	//加密
-	public static String encrypeByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        MessageDigest md5=MessageDigest.getInstance("MD5");
-       
-        String encrypeStr = Base64Utils.encodeToString(md5.digest(str.getBytes("utf-8")));
-        return encrypeStr;
-    }
-	
+	private MD5Util() {
+	}
+
+	// 加密
+	public static String encrypeByMd5(String str) {
+		try {
+			MessageDigest md5 = MessageDigest.getInstance("MD5");
+			String encrypeStr = Base64Utils.encodeToString(md5.digest(str.getBytes("utf-8")));
+			return encrypeStr;
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw new RuntimeException("密码加密失败");
+		}
+
+	}
+
 	public static void main(String[] args) throws Exception {
 		System.out.println(encrypeByMd5("admin"));
 	}
-	
+
 }
